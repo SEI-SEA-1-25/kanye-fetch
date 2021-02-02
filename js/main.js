39f74aa5df80c49b405451bd104d4bbfb10ca702
mariiamel <mariiamelnikova96@gmail.com>
2021-02-01 22:49:21 -0800

    Kanye-fetch HW Mariia

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("👀")
+
+let h2El = document.querySelector('h2');
+
+let quoButton = document.querySelector('.quoButton');
+ quoButton.addEventListener('click', generateQuo)
+
+// //function 
+  function generateQuo(event) {
+    event.preventDefault;
+
+fetch('https://api.kanye.rest/') 
+    .then(function(response) {
+    return response.json();
+ })
+    .then(function(json) {
+    console.log(json);
+    let quote = json;
+
+    h2El.textContent = json.quote;
+
+ }) .catch(error => {
+    console.log(error)
+ }) 
+}
