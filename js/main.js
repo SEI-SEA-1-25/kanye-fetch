21bc65778598ec396dda5b0a29e9172fb915cd05
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-02-01 20:11:36 -0800

    got it working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("👀")
+let tweet = document.querySelector(".container");
+let reset = document.querySelector("button");
+
+reset.addEventListener("click", gimmeWisdom);
+
+function gimmeWisdom(event) {
+    event.preventDefault();
+    fetch("https://api.kanye.rest/")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        // console.log(json);
+        let random = json;
+        let randomWisdom = random.quote;
+        tweet.innerText = `"${randomWisdom}" -Yeezy`;
+        reset.innerText = "More wisdom plz";
+        document.querySelector(".container").style.textAlign = "center"
+      });
+  }
