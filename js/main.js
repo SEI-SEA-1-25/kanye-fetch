374fc817370715f56c1a3ddbe65800207c029b04
NBryant83 <nakisha.bryant@gmail.com>
2021-02-02 11:27:50 -0500

    kanye-fetch homework

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log("👀")
+// make a fetch request-get
+// .then() json
+// post data to main page
+// create button to start from step 1
+
+//let newQuote = document.querySelector('#button').value;
+button.addEventListener('click', quotes);
+
+function quotes() {
+
+    fetch('https://api.kanye.rest/')
+        .then(function(response) {
+            return response.json();
+
+        })
+        .then(function(json) {
+            let quotes = json.quote;
+            //console.log(quotes);
+            document.querySelector('#statement').innerText = quotes;
+
+        })
+
+    //console.log(newQuote);
+
+}
+
+
+//let h1El = document.querySelector('#statement');
