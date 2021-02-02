8391a438ea737596355debd5fabb98201d952432
dlashinsky <da.lashinsky@gmail.com>
2021-02-02 00:46:08 -0800

    finishing up HW.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("👀")
+//DOM Operators
+let buttonEl = document.querySelector(".newquote");
+let quoteEl= document.querySelector(".kanye-quote");
+
+
+//Page Start
+buttonEl.addEventListener('click', newQuote);
+
+
+
+function newQuote() {
+
+fetch('https://api.kanye.rest')
+.then(function(response) {
+    return response.json();
+ })
+    .then(function(json){
+    let newText = json.quote;
+    quoteEl.innerText = newText;
+ })
+
+}
+
