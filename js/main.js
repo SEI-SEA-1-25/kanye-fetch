37d7b294fd68a521fe735e50f5c0739120a5da21
AlrightErin27 <erinmontybruce@gmail.com>
2021-02-01 17:51:42 -0800

    initail html & css. JS is console logging random Kanye gibberish.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("👀")
+// DOM references
+// let postContainer = document.querySelector('#post-container');
+//Vars
+//DOM References
+//Event Listeners
+//Functions
+fetch("https://api.kanye.rest")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    console.log(json);
+  });
