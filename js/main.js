9a2aad0c72a8a4829031428472c7771b69f6efea
AlrightErin27 <erinmontybruce@gmail.com>
2021-02-01 18:41:31 -0800

    JS says random quote, but no reset. Going on to try Sara's idea.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-// DOM references
-// let postContainer = document.querySelector('#post-container');
-//Vars
-//DOM References
-//Event Listeners
-//Functions
+let resetButton = document.querySelector("button");
+
 fetch("https://api.kanye.rest")
   .then(function (response) {
     return response.json();
   })
   .then(function (json) {
     console.log(json);
+    let randomQuote = json.quote;
+    createEl(randomQuote);
   });
+
+//new function
+function createEl(randomQuote) {
+  let divEL = document.createElement("div");
+  let h2El = document.createElement("h2");
+  document.querySelector(".quoteWrap").appendChild(divEL);
+  divEL.appendChild(h2El).innerText = `${randomQuote}`;
+
+  return divEL;
+}
+resetButton.addEventListener("click", createEl);
