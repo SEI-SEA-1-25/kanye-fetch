53dc2a619d991b9a971427e78d8bfa6c81176710
kchu01 <chukathy01@gmail.com>
2021-02-01 22:21:52 -0800

    Create Kanye Edit 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log("👀")
+// console.log("👀")
+
+let quoteContainer = document.querySelector('#quote-container')
+
+const kanyeQuotes = () => {
+
+    fetch(`https://api.kanye.rest/`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+    console.log(json);
+    createQuote()
+    let quote = json.quote;
+    
+    })
+
+}
+
+const createQuote = () => {
+    let h2El = document.createElement('h2')
+    h2El.textContent = quote;
+    
+    while(quoteContainer.firstChild) {
+    quoteContainer.removeChild(quoteContainer.firstChild)
+   }
+}
+
+
+// const createQuote = () => {
+//     let divEl = document.createElement('div')
+
+//     let h2El = document.createElement('h2')
+//     h2El.textContent = quote;
+
+//     divEl.appendChild(h2El)
+
+//     return divEl
+// }
+
